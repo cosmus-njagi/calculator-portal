e057997d8be2b1ac3e9c9bbdc64eda23592d4729
Cosmus <njagicosmus@gmail.com>
2023-06-24 22:51:49 +0300

    default styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { NgxUiLoaderModule, NgxUiLoaderConfig } from 'ngx-ui-loader';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth-guard/auth.guard';
+import { MatIconModule } from '@angular/material/icon';
+
+const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+  fgsType: 'circle',
+  fgsColor: '#000000',
+  fgsSize: 60,
+  overlayColor: 'rgba(0, 0, 0, 0.5)',
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent],
+  providers: [AuthGuard],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatDatepickerModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    HttpClientModule,
+    MatIconModule,
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LoginComponent],
 })
-export class AppModule { }
+export class AppModule {}
