532ce177340e24d78836ecc2d422bfb68724ee55
Cosmus <njagicosmus@gmail.com>
2023-06-24 18:06:03 +0300

    added more design to login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import { ApiService } from 'src/services/api-service.service';
 
 @Component({
-  selector: 'login.component.html',
+  selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  signInForm: FormGroup;
+  hidePassword: boolean = true;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private apiService: ApiService
+  ) {
+    this.signInForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {}
+
+  //toggle password visibility
+  togglePasswordVisibility(): void {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  submitLogin() {
+    console.log(this.signInForm.value);
+    const endpoint = environment.url.login;
+    const data = this.signInForm.value;
+    this.apiService.post(endpoint, data).subscribe({
+      next: (response) => {
+        // Handle the successful response
+        console.log(response);
+      },
+      error: (error) => {
+        // Handle the error
+        console.error(error);
+      },
+    });
+  }
 }
