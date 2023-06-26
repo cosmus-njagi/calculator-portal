4d102b470822fc6421d85207da775710ebb4de39
Cosmus <njagicosmus@gmail.com>
2023-06-26 05:15:01 +0300

    table for displaying tabulated data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatTableDataSource } from '@angular/material/table';
 import { ApiService } from 'src/services/api-service.service';
 import { LoaderService } from 'src/services/loader-service.service';
 import { environment } from '../../environments/environment';
+import { AuthService } from 'src/services/auth-service.service';
+import { PdfDownloadService } from 'src/services/PdfDownloadService.service';
+import { CurrencyFormatPipe } from '../custom-pipe/currency-format.pipe';
+import { AlertService } from 'src/services/alert-service.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -11,30 +14,47 @@ import { environment } from '../../environments/environment';
 })
 export class DashboardComponent implements OnInit {
   calculatorForm: FormGroup;
+  displayedColumns: string[] = [
+    'installmentNumber',
+    'startingBalance',
+    'installmentAmount',
+    'interestAmount',
+    'dueDate',
+    'totalRepayment',
+  ];
+  installments: any;
+  showTable: boolean = false;
+  exciseDuty: number = 0;
+  interestRate: number = 0;
+  legalFees: number = 0;
+  takeHomeAmount: number = 0;
+  processingFees: number = 0;
 
   constructor(
     private formBuilder: FormBuilder,
     private loaderService: LoaderService,
-    private apiService: ApiService
+    private apiService: ApiService,
+    private authService: AuthService,
+    private pdfDownloadService: PdfDownloadService,
+    private currencyFormatPipe: CurrencyFormatPipe,
+    private alertService: AlertService
   ) {
     this.calculatorForm = this.formBuilder.group({
-      amount: ['', Validators.required],
+      amount: ['', [Validators.required, Validators.min(50000)]],
       paymentFrequency: ['', Validators.required],
-      loanPeriod: ['', Validators.required],
+      loanPeriod: ['', [Validators.required, Validators.max(12)]],
       startDate: ['', Validators.required],
       interestType: ['', Validators.required],
     });
   }
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
-  // @ViewChild(MatPaginator) paginator: MatPaginator;
-
-  ngAfterViewInit() {
-    // this.dataSource.paginator = this.paginator;
-  }
+  ngAfterViewInit() {}
   ngOnInit() {
     console.log('opening dashboard');
+  }
+
+  logOutFunction() {
+    this.authService.logout();
   }
 
   submitForm() {
@@ -43,10 +63,16 @@ export class DashboardComponent implements OnInit {
     console.log('form values ###', this.calculatorForm.value);
     const endpoint = environment.url.calculator;
     const data = this.calculatorForm.value;
-    this.apiService.post(endpoint, data).subscribe({
-      next: (response) => {
+    this.apiService.postWithHeaders(endpoint, data).subscribe({
+      next: (response: any) => {
         // Handle the successful response
-        console.log(response);
+        this.showTable = true;
+        this.installments = response['installments'];
+        this.exciseDuty = response['exciseDuty'];
+        this.interestRate = response['interestRate'] * 100;
+        this.legalFees = response['legalFees'];
+        this.processingFees = response['processingFees'];
+        this.takeHomeAmount = parseFloat(response['takeHomeAmount']);
       },
       error: (error) => {
         // Handle the error
@@ -54,33 +80,48 @@ export class DashboardComponent implements OnInit {
       },
     });
   }
-}
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
-];
+  //export data to pdf
+  exportTableAsPdf() {
+    let tableHeaders = [
+      'No.',
+      '	Due Date',
+      '	Starting Balance',
+      '	Installment Amount',
+      'Interest Amount',
+
+      '	Total Repayment',
+    ];
+    let description = [
+      {
+        label: 'Excise duty',
+        value: this.currencyFormatPipe.transform(this.exciseDuty),
+      },
+      { label: 'Interest Rate', value: this.interestRate },
+      {
+        label: 'Legal Fees',
+        value: this.currencyFormatPipe.transform(this.legalFees),
+      },
+      {
+        label: 'Processing Fees',
+        value: this.currencyFormatPipe.transform(this.processingFees),
+      },
+      {
+        label: 'Take Home Amount',
+        value: this.currencyFormatPipe.transform(this.takeHomeAmount),
+      },
+    ];
+
+    this.pdfDownloadService.downloadPDF(
+      description,
+      this.installments,
+      tableHeaders,
+      'loan_calculator.pdf'
+    );
+  }
+
+  //send pdf to email
+  sendEmail() {
+    this.alertService.sendEmail();
+  }
+}
