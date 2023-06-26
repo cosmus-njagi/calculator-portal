49e5db30fc61f17322ec8fd484ee4497640653ca
Cosmus <njagicosmus@gmail.com>
2023-06-26 04:52:35 +0300

    download service

---
+++ src/services/PdfDownloadService.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+import { TDocumentDefinitions, TableCell } from 'pdfmake/interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PdfDownloadService {
+  constructor() {
+    (pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
+  }
+
+  downloadPDF(
+    description: any,
+    data: any[],
+    headers: string[],
+    filename: string
+  ) {
+    const documentDefinition: TDocumentDefinitions = {
+      pageOrientation: 'landscape',
+      content: [
+        { text: 'Loan_Calculator', style: 'header' },
+        {
+          ul: description.map((item: any) => [
+            { text: `${item.label}: ${item.value}` },
+            '\n', // Add a blank line after each item
+          ]),
+        },
+        {
+          table: {
+            headerRows: 1,
+            widths: Array(headers.length).fill('auto'),
+            body: [
+              headers,
+              ...data.map((item) => Object.values(item) as TableCell[]),
+            ],
+          },
+        },
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          margin: [0, 0, 0, 10],
+        },
+        tableStyle: {
+          font: 'Roboto-Bold.ttf',
+        },
+      },
+    };
+
+    const pdfDocGenerator = pdfMake.createPdf(documentDefinition);
+    pdfDocGenerator.download(filename);
+  }
+}
